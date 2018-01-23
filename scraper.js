24c68a7ffbbbecbc0e1f281a585943a7e4804dc7
Mraleksa <a.gribanovskiy@gmail.com>
2018-01-23 17:33:40 +0200

    Update scraper.js

--- scraper.js
+++ scraper.js
@@ -1,59 +1,156 @@
-// This is a template for a Node.js scraper on morph.io (https://morph.io)
-
-var cheerio = require("cheerio");
-var request = require("request");
+var client = require('http-api-client');
+var d3 = require("d3");
 var sqlite3 = require("sqlite3").verbose();
+var db = new sqlite3.Database("data.sqlite");
 
-function initDatabase(callback) {
-	// Set up sqlite database.
-	var db = new sqlite3.Database("data.sqlite");
-	db.serialize(function() {
-		db.run("CREATE TABLE IF NOT EXISTS data (name TEXT)");
-		callback(db);
-	});
-}
 
-function updateRow(db, value) {
-	// Insert some data.
-	var statement = db.prepare("INSERT INTO data VALUES (?)");
-	statement.run(value);
-	statement.finalize();
-}
+var formatTime = d3.timeFormat("%Y-%m-%d");
 
-function readRows(db) {
-	// Read some data.
-	db.each("SELECT rowid AS id, name FROM data", function(err, row) {
-		console.log(row.id + ": " + row.name);
-	});
-}
 
-function fetchPage(url, callback) {
-	// Use request to read in pages.
-	request(url, function (error, response, body) {
-		if (error) {
-			console.log("Error requesting page: " + error);
-			return;
+db.each("SELECT dateModified FROM data ORDER BY dateModified DESC LIMIT 1", function(err, timeStart) {
+//var start =  "2017-01-01T10:18:57.452368+03:00"
+var start = timeStart.dateModified
+var end  = formatTime(new Date());
+//var end  = "2017-01-03"
+console.log("старт full: "+start); 
+
+
+console.log("старт: "+start.replace(/T.*/, "") +"конец: "+end); 
+
+function piv(){  
+//p++;
+client.request({url: 'https://public.api.openprocurement.org/api/2.3/contracts?offset='+start})
+      .then(function (data) {
+		var dataset = data.getJSON().data;
+		start = data.getJSON().next_page.offset;			
+		console.log(start)
+		return dataset;
+	})	
+	.then(function (dataset) {			
+		dataset.forEach(function(item) {
+		client.request({url: 'https://public.api.openprocurement.org/api/2.3/contracts/'+item.id})
+		.then(function (data) {	
+
+	//var change = data.getJSON().data.changes[data.getJSON().data.changes.length-1].rationaleTypes[0];
+	var changeLength = data.getJSON().data.changes.length;
+	
+	//if(change=="itemPriceVariation"){		
+		var up=0;var down=0;
+		for (var p = 0; p < changeLength; p++) {
+		if(data.getJSON().data.changes[p].rationaleTypes[0]=="itemPriceVariation"){
+			up=up+1;
+		}
+		if(data.getJSON().data.changes[p].rationaleTypes[0]=="priceReduction"){
+			down=down+1;
+		}
 		}
 
-		callback(body);
+	if(up>0){
+	
+		var upDates="";var downDate="";
+		for (var p = 0; p < changeLength; p++) {
+		if(data.getJSON().data.changes[p].rationaleTypes[0]=="itemPriceVariation"){
+			upDates = upDates+data.getJSON().data.changes[p].dateSigned+";"
+		}
+		if(data.getJSON().data.changes[p].rationaleTypes[0]=="priceReduction"){
+			downDate = downDate+data.getJSON().data.changes[p].dateSigned+";"
+		}	
+		}
+			
+//console.log(changeLength+"-"+upDate);
+	
+	if(data.getJSON().data.changes[0].rationaleTypes[0]=="itemPriceVariation")
+		{
+			var first = data.getJSON().data.changes[0].dateSigned;
+		}
+ 	
+	var tender_id = data.getJSON().data.tender_id;
+	var id = data.getJSON().data.id;		
+	var lotIdContracts = data.getJSON().data.items[0].relatedLot;
+	var dateSigned = data.getJSON().data.dateSigned;
+	var amount = data.getJSON().data.value.amount;	
+	var region = data.getJSON().data.procuringEntity.address.region;	
+	
+	
+		//////////save//////////////
+		client.request({url: 'https://public.api.openprocurement.org/api/2.3/tenders/'+data.getJSON().data.tender_id})
+		.then(function (data) {
+		var startAmount;
+		if(data.getJSON().data.lots==undefined){
+			startAmount = data.getJSON().data.value.amount;
+		}
+		else {
+		for (var i = 1; i <= data.getJSON().data.lots.length; i++) {
+				if(lotIdContracts==data.getJSON().data.lots[data.getJSON().data.lots.length-(i)].id){
+				startAmount =  data.getJSON().data.lots[data.getJSON().data.lots.length-(i)].value.amount
+				};			
+			}
+		}
+		var save=Math.round((startAmount-amount)/startAmount*100);
+		var numberOfBids;
+		if(isNaN(data.getJSON().data.numberOfBids)){numberOfBids = 1}
+		else {numberOfBids=data.getJSON().data.numberOfBids};
+		
+		//////////SQLite//////////////
+	db.serialize(function() {	
+	db.run("CREATE TABLE IF NOT EXISTS data (dateModified TEXT,id TEXT,dateSigned TEXT,first TEXT,tenderID TEXT,procuringEntity TEXT,numberOfBids INT,startAmount INT,amount INT,save INT,cpv TEXT,region TEXT,up INT,down INT,downDate TEXT,upDates TEXT)");
+	var statement = db.prepare("INSERT INTO data VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)"); 	
+	statement.run(
+	item.dateModified,
+	id,
+	dateSigned,
+	first,
+	data.getJSON().data.tenderID,
+	data.getJSON().data.procuringEntity.name.toUpperCase(),
+	numberOfBids,
+	startAmount,
+	amount,
+	save,
+	data.getJSON().data.items[0].classification.description,
+	region,
+	up,
+	down,
+	downDate,
+	upDates
+	);
+	statement.finalize();
 	});
+	//////////SQLiteEnd//////////////
+	
+		})
+		.catch(function  (error) {									
+		}); 
+		//////////saveEnd//////////////
+	
+	
+	}		
+	})
+	.catch(function  (error) {
+		//console.log("error_detale2")				
+	});  
+	});	
+	})
+	.catch(function  (error) {
+		//console.log("error_detale3")				
+	})
+	.then(function () {	
+	
+		if (start.replace(/T.*/, "") != end) {
+			setTimeout(function() {piv ();},10000);
+		}	
+		else {
+			console.log("STOP")
+		}
+		
+		})
+	.catch( function (error) {
+		console.log("error")
+		piv ();
+	});   					
 }
 
-function run(db) {
-	// Use request to read in pages.
-	fetchPage("https://morph.io", function (body) {
-		// Use cheerio to find things in the page with css selectors.
-		var $ = cheerio.load(body);
 
-		var elements = $("div.media-body span.p-name").each(function () {
-			var value = $(this).text().trim();
-			updateRow(db, value);
-		});
 
-		readRows(db);
+piv ();	
 
-		db.close();
-	});
-}
-
-initDatabase(run);
+})
